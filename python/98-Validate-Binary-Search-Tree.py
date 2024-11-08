8f6d9e5e0f1a046f35db7ffc3c6cc7a8cd5e1f54
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2024-11-08 19:35:53 +0600

    added solution for 98

---
+++ python/98-Validate-Binary-Search-Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def is_valid(node, minn, maxx):
+            if not node:
+                return True
+            
+            if node.val <= minn or node.val >= maxx:
+                return False
+            
+            return is_valid(node.left, minn, node.val) and is_valid(node.right, node.val, maxx)
+        
+        return is_valid(root, float("-inf"), float("inf"))
+
+
+# Tutorial link: https://www.youtube.com/watch?v=nTXuWuqIka4&t
+# Time complexity: O(n)
+# Space complexity: O(h) / O(n). h = height of the tree
+        
